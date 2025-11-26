e007bafb6167a20bad0207108ef26ad5e09037bb
Verkury <aleksandrlaty78@gmail.com>
2025-11-26 11:33:38 +0700

    Developed processing files and connect to engine

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"fmt"
 	_ "fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	
+	var filePath string
+	// Check errors 
+	if (len(os.Args) > 1) {
+		filePath = os.Args[1]
+	} else {
+		filePath = ""
+	}
+
+	if filePath != "" {
+		checkFile(filePath) // Checking file
+		initialization(filePath) // Starting program
+	}
+
+	initialization("") // Starting program without file
+}
+
+func checkFile(path string) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Printf("Ошибка при получении аболютного пути %v\n", err)
+		return 
+	}
+
+	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+		fmt.Printf("Файл не существует: %s\n", absPath)
+		return 
+	}
+
+	path = absPath
 }

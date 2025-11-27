60f33367c830c27a3b92afdddface88c455bb94c
Verkury <aleksandrlaty78@gmail.com>
2025-11-27 22:50:43 +0700

    Reanamed AdvancedPrint to Prints

---
+++ Prints/prints.go
@@ -0,0 +1,48 @@
+package Prints
+
+import (
+	"fmt"
+	"time"
+)
+
+func PrintLine(line string, delay int) {
+	for v := range line {
+		fmt.Print(v)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+	}
+	fmt.Print("\n")
+}
+
+func PrintLineCenter(line string, delay int, width int) {
+	indent := (width - len(line)) / 2
+
+	for i := 0; i < indent; i++ {
+		fmt.Print(" ")
+	}
+	
+	PrintLine(line, delay)
+}
+
+func PrintLinesCenter(lines []string, delay int, width int) {
+	for _, line := range lines {
+		PrintLineCenter(line, delay, width)
+	}
+}
+
+func SplitText(text string, width, procent int) []string { // Spliting text to send in print func
+	lenght := width * procent
+	
+	runes := []rune(text)
+	lines := make([]string, len(text) / lenght + 3)
+
+	for _, value := range runes {
+		var line string
+		if len(line) <= lenght  && value != ' ' {
+			line += string(value)
+		} else {
+			lines = append(lines, line)
+			line = ""
+		}
+	} 
+	return lines
+}
